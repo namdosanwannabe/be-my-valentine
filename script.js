225a33eff40b7fd8cc3fb7d67714c84fe2f0fe9f
Ser <159701178+Sergitowo@users.noreply.github.com>
2024-02-11 09:11:51 -0500

    aea

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ noButton.addEventListener("click", function () {
 });
 
 function handleYesClick() {
-  titleElement.innerHTML = "Yayyy!! :3";
+  titleElement.innerHTML = "Siiiiiiii te amooo!! :3";
   buttonsContainer.classList.add("hidden");
   changeImage("yes");
 }
@@ -42,12 +42,12 @@ function resizeYesButton() {
 
 function generateMessage(noCount) {
   const messages = [
-    "No",
-    "Are you sure?",
-    "Pookie please",
-    "Don't do this to me :(",
-    "You're breaking my heart",
-    "I'm gonna cry...",
+    "No :c",
+    "No tilina?",
+    "Por favorrr",
+    "Me pongo triste :(",
+    ":c",
+    "Plisssssss...",
   ];
 
   const messageIndex = Math.min(noCount, messages.length - 1);
